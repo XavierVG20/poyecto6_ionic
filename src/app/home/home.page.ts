e5aff221760471b2d2fb5906aeee877e4e75cf9f
XavierVG20 <kjaviervelasque@hotmail.com>
2023-02-14 21:09:30 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
+import {map} from 'rxjs/operators'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  datas : any =[];
+  constructor(private http: HttpClient) {}
+  ngOnInit(){
+    this.getdata().subscribe(res=>{
+      console.log(res);
+      this.datas = res;
+    });
+  }
 
-  constructor() {}
+  getdata(){
+    return this.http
+    .get("assets/api.json")
+    .pipe(
+      map((res:any)=>{
+        return res.data;
+      })
+    )
+  }
 
 }
